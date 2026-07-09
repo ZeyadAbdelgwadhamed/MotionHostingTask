4d3d333ece56ee26e3b28189a385d5778f223a8a
zeyadabdelgwad <zeyadabdelgawad17@gmail.com>
2026-07-09 16:27:09 +0300

    add some locator and open domain page methode

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,11 +1,13 @@
 package Pages;
 
+import Utilities.ActionsElement;
+import Utilities.Scrolling;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
 
-    private WebDriver driver;
+    private final WebDriver driver;
 
 
     // Constructor
@@ -18,14 +20,18 @@ public class HomePage {
 
     // Locators
 
-    private By domainsMenu = By.xpath("//a[contains(text(),'Domains')]");
-
-    private By webHostingMenu = By.xpath("//a[contains(text(),'Web Hosting')]");
+    private final By cookiesButton =By.cssSelector("button[id='onetrust-accept-btn-handler']");
+    private final By domainsMenu = By.xpath("//a[contains(text(),'Domains')]");
+    private final By webHostingMenu = By.xpath("//a[contains(text(),'Web Hosting')]");
+    private final By domainName= By.xpath("//*[@id=\"imh-footer-navigation\"]/nav/div/div/div[3]/ul/li[14]/a");
 
 
 
     // Actions
 
+    public void acceptAllCookies(){
+        ActionsElement.clickOnElement(driver,cookiesButton);
+    }
 
     public String getHomePageTitle(){
 
@@ -41,5 +47,14 @@ public class HomePage {
     }
 
 
+    public boolean isWebHostingMenuDisplayed(){
+
+        return driver.findElement(webHostingMenu).isDisplayed();
+
+    }
+    public void openDomainPage(){
+        Scrolling.scrollToElement(driver,domainName);
+        ActionsElement.clickOnElement(driver,domainName);
+    }
 
 }
